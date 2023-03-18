c4e7dbf746be134918ad630defd6db4209cb8cd2
stto1210@gmail.com <stto1210@gmail.com>
2023-03-18 19:25:12 +0100

    starte å hente status fra lader. mangler å skrive objekt å mappe retur til

--- src/main/java/no/steintokvam/smartcharger/easee/EaseeService.kt
+++ src/main/java/no/steintokvam/smartcharger/easee/EaseeService.kt
@@ -8,20 +8,29 @@ import okhttp3.Request
 class EaseeService {
 
     private val BASE_URL = "https://api.easee.cloud/api"
+    private val client = OkHttpClient()
+    private val mapper = ObjectMapper()
 
     fun getChargerId(): List<Charger> {
+        val request = createRequest("/charger")
 
-        val client = OkHttpClient()
+        val response = client.newCall(request).execute()
 
-        val request = Request.Builder()
-                .url("$BASE_URL/chargers")
+        return mapper.readValue(response.body?.charStream()?.readText(),  mapper.typeFactory.constructCollectionType(List::class.java, Charger::class.java))
+    }
+
+    fun getChargerState(chargerID: String) {
+        val request = createRequest("/chargers/$chargerID/state")
+        val response = client.newCall(request).execute()
+        //mapper.readValue(response.body?.charStream()?.readText(),)
+    }
+
+    private fun createRequest(endpoint: String): Request {
+        return Request.Builder()
+                .url(BASE_URL+endpoint)
                 .get()
                 .addHeader("accept", "application/json")
                 .addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImN0eSI6IkpXVCJ9.eyJBY2NvdW50SWQiOjU1MzE4LCJVc2VySWQiOjQyMDU4LCJ1bmlxdWVfbmFtZSI6IlN0ZWluIFBldHRlciBUb2t2YW0iLCJyb2xlIjoiVXNlciIsIm5iZiI6MTY3OTE1OTI1NCwiZXhwIjoxNjc5MjQ1NjU0LCJpYXQiOjE2NzkxNTkyNTR9.E2FUDC16THRbNSgCss-On-Frs5ZssbCrxFm275skwNA")
                 .build()
-
-        val response = client.newCall(request).execute()
-        val mapper = ObjectMapper()
-        return mapper.readValue(response.body?.charStream()?.readText(),  mapper.typeFactory.constructCollectionType(List::class.java, Charger::class.java))
     }
 }
--- src/test/java/no/steintokvam/smartcharger/easee/EaseeServiceTest.kt
+++ src/test/java/no/steintokvam/smartcharger/easee/EaseeServiceTest.kt
@@ -4,12 +4,19 @@ import org.junit.jupiter.api.Test
 import kotlin.test.assertTrue
 
 class EaseeServiceTest {
+    private val easeeService = EaseeService()
 
     @Test
     fun testGetChargers() {
-        val easeeService = EaseeService()
+
         val chargers = easeeService.getChargerId()
 
         assertTrue(chargers[0].id == "EHE6ZQU7")
+    }
+
+    @Test
+    fun testGetChargerState(){
+        val chargerID = "EHE6ZQU7"
+        easeeService.getChargerState(chargerID)
     }
 }
